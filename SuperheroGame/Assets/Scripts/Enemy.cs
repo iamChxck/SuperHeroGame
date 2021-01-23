dadf8c5dc4f04bf0b34f71c7cbbf806e6204ef80
Joshua Lemon <joshualemon249@gmail.com>
2021-01-24 02:38:56 +0800

    Added Enemy and playerDeath(check player code)/enemyDeath

---
+++ SuperheroGame/Assets/Scripts/Enemy.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    SphereCollider sphereCollider;
+    [SerializeField] public int enemyHealth;
+    [SerializeField] public int enemyDamage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sphereCollider = GetComponent<SphereCollider>();
+        enemyHealth = 1;
+        enemyDamage = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        EnemyDeath();
+    }
+
+    void EnemyDeath()
+    {
+        if(enemyHealth <= 0)
+        {
+            Destroy(this.gameObject);
+            Debug.Log("Enemy Died!");
+        }
+    }
+}
---
+++ SuperheroGame/Assets/Scripts/Player.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Enemy enemy;
+    CapsuleCollider capsuleCollider;
+    [SerializeField] int playerDamage;
+    [SerializeField] int playerHealth;
+    [SerializeField] bool isEnemyInRange;
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        capsuleCollider = GetComponent<CapsuleCollider>();
+        playerDamage = 1;
+        isEnemyInRange = false;
+        playerHealth = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        BasicAttack();
+        PlayerDeath();
+    }
+
+    void BasicAttack()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Pressed LMB");
+            if (isEnemyInRange)
+            {
+                enemy.enemyHealth -= playerDamage;
+                Debug.Log("Enemy got hit!");
+            }
+        }
+    }
+
+    void PlayerDeath()
+    {
+        if (playerHealth <= 0)
+        {
+            Destroy(this.gameObject);
+            Debug.Log("Player has Died!");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            enemy = other.GetComponent<Enemy>();
+            Debug.Log("Enemy in range");
+            isEnemyInRange = true;
+            /*TEST FOR PLAYER DEATH BELOW*/
+            Debug.Log("Here");
+            //playerHealth -= enemy.enemyDamage;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        
+        isEnemyInRange = false;
+        Debug.Log("Enemy out of range");
+    }
+
+   
+}
